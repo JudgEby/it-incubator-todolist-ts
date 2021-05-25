85a794feeadf982ef3c5d025d5e1fdb54c7f5911
Yan Dobrovolskiy <bloodwoodwood@gmail.com>
2021-05-25 16:05:24 +0300

    Completed the first lesson

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,25 @@
-import React from "react";
-import "./App.css";
-import Todolist from "./Todolist";
+import React from 'react'
+import './App.css'
+import Todolist from './Todolist'
 
 function App() {
+  const task1 = [
+    { id: 1, title: 'HTML&CSS', isDone: true },
+    { id: 2, title: 'JS', isDone: true },
+    { id: 3, title: 'ReactJS', isDone: false },
+  ]
+  const task2 = [
+    { id: 1, title: 'Hello World', isDone: true },
+    { id: 2, title: 'I am Happy', isDone: false },
+    { id: 3, title: 'Yo', isDone: false },
+  ]
+
   return (
-    <div className="App">
-      <Todolist />
-      <Todolist />
-      <Todolist />
+    <div className='App'>
+      <Todolist title={'What to learn'} tasks={task1} />
+      <Todolist title={'Songs'} tasks={task2} />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/Todolist.tsx
+++ src/Todolist.tsx
@@ -1,22 +1,36 @@
-import React from "react";
+import React from 'react'
 
-const Todolist = () => {
+type PropsType = {
+  title: string
+  tasks: Array<TaskType>
+}
+
+type TaskType = {
+  id: number
+  title: string
+  isDone: boolean
+}
+
+const Todolist = (props: PropsType) => {
   return (
     <div>
-      <h3>What to learn</h3>
+      <h3>{props.title}</h3>
       <div>
         <input />
         <button>+</button>
       </div>
       <ul>
         <li>
-          <input type="checkbox" checked={true} /> <span>HTML&CSS</span>
+          <input type='checkbox' checked={props.tasks[0].isDone} />{' '}
+          <span>{props.tasks[0].title}</span>
         </li>
         <li>
-          <input type="checkbox" checked={true} /> <span>JS</span>
+          <input type='checkbox' checked={props.tasks[1].isDone} />{' '}
+          <span>{props.tasks[1].title}</span>
         </li>
         <li>
-          <input type="checkbox" checked={false} /> <span>React</span>
+          <input type='checkbox' checked={props.tasks[2].isDone} />{' '}
+          <span>{props.tasks[2].title}</span>
         </li>
       </ul>
       <div>
@@ -25,7 +39,7 @@ const Todolist = () => {
         <button>Completed</button>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default Todolist;
+export default Todolist
