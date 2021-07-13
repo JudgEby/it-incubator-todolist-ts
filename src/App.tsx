5b8ebf8393552a1b64d4ec98012d20a691c0ff44
Yan Dobrovolskiy <bloodwoodwood@gmail.com>
2021-07-13 22:35:39 +0300

    completed lesson 8

--- src/App.tsx
+++ src/App.tsx
@@ -17,7 +17,7 @@ import { Menu } from '@material-ui/icons'
 
 export type FilterValuesType = 'all' | 'active' | 'completed'
 
-type TodoListType = {
+export type TodoListType = {
   id: string
   title: string
   filter: FilterValuesType
@@ -166,6 +166,7 @@ function App() {
           {todoListsComponents}
         </Grid>
       </Container>
+      <p>Hi</p>
     </div>
   )
 }
